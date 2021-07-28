820f14dce4afa7a930929bea6b0731f60ee78d0f
= <=>
2021-07-28 21:53:35 +0100

    Initial commit

---
+++ src/test/java/com/phoebelord/petshop/PetShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phoebelord.petshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
